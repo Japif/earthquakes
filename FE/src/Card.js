f5b79c96bf791d423b4382978b5f887e16f9daaf
Jacopo <francavillajacopo@gmail.com>
2022-09-29 18:21:31 +0200

    fixed list

--- FE/src/Card.js
+++ FE/src/Card.js
@@ -2,19 +2,42 @@ import classes from "./Card.module.css";
 import Button from "./components/Button";
 import Label from "./components/Label";
 
-function Card() {
+const mapsUrl = (lat, lon) => {
+  return `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`;
+};
+
+const hearUrl = (url) => {
+  const id = url.split("/").pop();
+  return `https://www.hsit.it/xml-server.php?item=quest&action=compile&event_id=${id}&output=int`;
+};
+
+function Card(props) {
   return (
     <div className={classes.container}>
       <div>
-        <Label icon="location_on">Location: Rome</Label>
-        <Label icon="schedule">Time: 21/09/2022 09:15</Label>
-        <Label icon="arrow_downward">Depth: 12km</Label>
-        <Label icon="language">Coordinates: 41.2, 12.6</Label>
+        <Label icon="location_on">Location: {props.data.place}</Label>
+        <Label icon="schedule">Time: {props.data.date}</Label>
+        <Label icon="arrow_downward">Depth: {props.data.ipo}km</Label>
+        <Label icon="language">
+          Coordinates: {props.data.lat}, {props.data.lon}
+        </Label>
       </div>
       <div className={classes["button-container"]}>
-        <Button style={{borderColor: `#d9dfe6`, backgroundColor: `white`}}>Open details</Button>
-        <Button style={{borderColor: `#d9dfe6`, backgroundColor: `white`}}>View on Google Maps</Button>
-        <Button style={{}}>Did you hear it?</Button>
+        <Button
+          url={props.data.url}
+          style={{ borderColor: `#d9dfe6`, backgroundColor: `white` }}
+        >
+          Open details
+        </Button>
+        <Button
+          url={mapsUrl(props.data.lat, props.data.lon)}
+          style={{ borderColor: `#d9dfe6`, backgroundColor: `white` }}
+        >
+          View on Google Maps
+        </Button>
+        <Button url={hearUrl(props.data.url)} style={{}}>
+          Did you hear it?
+        </Button>
       </div>
     </div>
   );
--- FE/src/components/Button.js
+++ FE/src/components/Button.js
@@ -1,9 +1,17 @@
 import classes from "./Button.module.css";
 
+const openInNewTab = (url) => {
+  window.open(url, "_blank", "noopener,noreferrer");
+};
+
 function Button(props) {
   return (
-    <div className={classes.container} style={props.style}>
-        {props.children}
+    <div
+      onClick={() => openInNewTab(props.url)}
+      className={classes.container}
+      style={props.style}
+    >
+      {props.children}
     </div>
   );
 }
